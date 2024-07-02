4ded20916692652fb6b6daf19562da078390698a
sarangbainalwar <145209224+sarangbainalwar@users.noreply.github.com>
2024-07-02 16:29:17 +0530

    comitted4

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -8,7 +8,7 @@ import TrackVisibility from "react-on-screen";
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Web Developer", "Java Coder", "REACT Designer"];
+    // const toRotate = ["Web Developer", "Java Coder", "REACT Designer"];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 500;
