ef6927e8a2e1ccb434088c4a7c57149175a1e68d
sarangbainalwar <145209224+sarangbainalwar@users.noreply.github.com>
2024-07-02 15:32:01 +0530

    Fix missing dependency in useEffect in Banner.js

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { useState, useEffect } from "react"; 
+import { useState, useEffect, useCallback } from "react"; 
 import { ArrowRightCircle } from "react-bootstrap-icons"; 
 import headerImg from '../assets/img/header-img.svg';
 // import { toBeInTheDOM } from "@testing-library/jest-dom/dist/matchers";
@@ -13,15 +13,7 @@ export const Banner = () => {
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 500;
 
-    useEffect(() => {
-        let ticker = setInterval(() => {
-            tick();
-        }, delta);
-
-        return () => { clearInterval(ticker) };
-    }, [text, delta]); // Add delta as a dependency
-
-    const tick = () => {
+    const tick = useCallback(() => {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i];
         let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
@@ -40,7 +32,15 @@ export const Banner = () => {
             setLoopNum(loopNum + 1);
             setDelta(500);
         }
-    };
+    }, [loopNum, isDeleting, text]);
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+
+        return () => { clearInterval(ticker); };
+    }, [tick, delta]); // Add tick and delta as dependencies
 
     return (
         <section className="banner" id="home">
