d6d66809308622d7834274614b039c66d9ba33f2
sarangbainalwar <145209224+sarangbainalwar@users.noreply.github.com>
2024-09-28 15:44:35 +0530

    ASA

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      { <Footer /> }
       {/* <Contact /> */}
       <EmailForm />
       <Newsletter/>
+      { <Footer /> }
     </div>
   );
 }
