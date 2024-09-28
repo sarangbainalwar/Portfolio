b26271b430e469e847f7e92a1f33a825bbf82670
sarangbainalwar <145209224+sarangbainalwar@users.noreply.github.com>
2024-09-28 22:56:38 +0530

    asd

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,63 +1,114 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 // import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useState, useEffect } from 'react'; 
-import logo from '../assets/img/logo-white.png';
-import navIcon1 from '../assets/img/nav-icon1.svg';
+import { useState, useEffect } from "react";
+import logo from "../assets/img/logo-white.png";
+import navIcon1 from "../assets/img/nav-icon1.svg";
 // import navIcon2 from '../assets/img/nav-icon2.svg';
 // import navIcon3 from '../assets/img/nav-icon3.svg';
-import resume from '../assets/img/resume.png';
-import git from '../assets/img/github-mark.svg';
+import resume from "../assets/img/resume.png";
+import git from "../assets/img/github-mark.svg";
 
 const NavigationBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        };
-        window.addEventListener("scroll", onScroll);
-        return () => window.removeEventListener("scroll", onScroll);
-    }, []);
-
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value); // This will set the active link state
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     };
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-    return (
-        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img src={logo} alt="Logo" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav">
-                    <span className='navbar-toggler-icon'></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-                    </Nav>
-                    <span className="navbar-text">
-                        <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/sarang-bainalwar-748a1822a/"><img src={navIcon1} alt="" /></a>
-                            <a href="https://github.com/sarangbainalwar"><img src={git} alt="" /></a>
-                            <a href="https://drive.google.com/file/d/1AcWtTwWbuRX9_W7zcWgw40CNvch_zEWv/view?usp=sharing"><img src={resume} alt="" /></a>
-                        </div>
-                        <button className="vvd" onClick={() => console.log('connect')} style={{ backgroundColor: '#E6E6FA' }}><a href="#emailcontact"><span>Let's Connect</span></a></button>
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value); // This will set the active link state
+  };
 
-                    </span>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  const scrollToContact = () => {
+    const contactSection = document.getElementById("emailcontact");
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a
+                href="https://www.linkedin.com/in/sarang-bainalwar-748a1822a/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon1} alt="LinkedIn profile" />
+              </a>
+              <a
+                href="https://github.com/sarangbainalwar"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={git} alt="GitHub profile" />
+              </a>
+
+              <a href="https://drive.google.com/file/d/1AcWtTwWbuRX9_W7zcWgw40CNvch_zEWv/view?usp=sharing">
+                <img src={resume} alt="" />
+              </a>
+            </div>
+            <button
+              className="vvd"
+              onClick={scrollToContact}
+              style={{ backgroundColor: "#E6E6FA" }}
+            >
+              <span>Let's Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavigationBar;
