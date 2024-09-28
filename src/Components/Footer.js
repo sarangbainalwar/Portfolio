073de94ec896b471a331ac6181a614db5bd56502
sarangbainalwar <145209224+sarangbainalwar@users.noreply.github.com>
2024-09-28 23:19:16 +0530

    asd

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -16,11 +16,22 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            <a
+                href="https://www.linkedin.com/in/sarang-bainalwar-748a1822a/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon1} alt="LinkedIn profile" />
+              </a>
+              <a
+                href="https://www.instagram.com/sarangbainalwar/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon3} alt="Instagram" />
+              </a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2024. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
